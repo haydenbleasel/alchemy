7af6fb3bab8cb225188a815b48bc121b1e5e6b53
Hayden Bleasel <hello@haydenbleasel.com>
2023-03-27 21:47:19 -0400

    Initial commit from Create Next App

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-sans)', ...defaultTheme.fontFamily.sans],
+        mono: ['var(--font-mono)', ...defaultTheme.fontFamily.mono],
+      },
+    },
+  },
+  plugins: [],
+};
