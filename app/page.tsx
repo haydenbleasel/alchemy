13998c3020fbce7bcde167b1f2685937e1ee26ac
Hayden Bleasel <hello@haydenbleasel.com>
2023-03-31 21:54:00 -0400

    Add cover photo and og metadata

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,19 @@ import type { FC } from 'react';
 export const metadata: Metadata = {
   title: 'The Alchemist',
   description: 'A visual journey of The Alchemist, powered by Midjourney.',
+  applicationName: 'Alchemy',
+  openGraph: {
+    locale: 'en_US',
+    url: process.env.NEXT_PUBLIC_SITE_URL,
+    images: [
+      {
+        url: `${process.env.NEXT_PUBLIC_SITE_URL}/cover.png`,
+        width: 1200,
+        height: 630,
+        alt: 'Alchemy',
+      },
+    ],
+  },
 };
 
 const Home: FC = () => <Artwork />;
