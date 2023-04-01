dfdc4c99813c75ed792a0027c106edbe5199cec8
Hayden Bleasel <hello@haydenbleasel.com>
2023-03-31 21:49:52 -0400

    Add metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import Artwork from '@/components/artwork';
+import type { Metadata } from 'next';
 import type { FC } from 'react';
+
+export const metadata: Metadata = {
+  title: 'The Alchemist',
+  description: 'A visual journey of The Alchemist, powered by Midjourney.',
+};
 
 const Home: FC = () => <Artwork />;
 
