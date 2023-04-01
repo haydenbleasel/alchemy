796773da73164c2dcf1f16260dd5fe6018c4cece
Hayden Bleasel <hello@haydenbleasel.com>
2023-03-31 21:04:27 -0400

    Add Pyramids, switch to JPGs

--- lib/artwork.ts
+++ lib/artwork.ts
@@ -1,78 +1,78 @@
 const artworks = [
   {
     name: 'The Abandoned Church',
-    image: '/artwork/church.png',
+    image: '/artwork/church.jpg',
     text: "The boy's name was Santiago. Dusk was falling as the boy arrived with his herd at an abandoned church. The roof had fallen in long ago, and an enormous sycamore had grown on the spot where the sacristy had once stood. He decided to spend the night there. He saw to it that all the sheep entered through the ruined gate, and then laid some planks across it to prevent the flock from wandering away during the night. There were no wolves in the region, but once an animal had strayed during the night, and the boy had had to spend the entire next day searching for it...",
   },
   {
     name: 'The Andalusian Marketplace',
-    image: '/artwork/andalusian-marketplace.png',
+    image: '/artwork/andalusian-marketplace.jpg',
     text: 'For the past few days he had spoken to them about only one thing: the girl, the daughter of a merchant who lived in the village they would reach in about four days. He had been to the village only once, the year before. The merchant was the proprietor of a dry goods shop, and he always demanded that the sheep be sheared in his presence, so that he would not be cheated.',
     quote:
       'It’s the possibility of having a dream come true that makes life interesting.',
   },
   {
     name: "The Gypsy's Tent",
-    image: '/artwork/gypsy.png',
+    image: '/artwork/gypsy.jpg',
     text: 'The old woman led the boy to a room at the back of her house; it was separated from her living room by a curtain of colored beads. The room’s furnishings consisted of a table, an image of the Sacred Heart of Jesus, and two chairs. The woman sat down, and told him to be seated as well. Then she took both of his hands in hers, and began quietly to pray.',
     quote:
       'At a certain point in our lives, we lose control of what’s happening to us, and our lives become controlled by fate. That’s the world’s greatest lie.',
   },
   {
     name: 'Travelling to Tangiers',
-    image: '/artwork/travel.png',
+    image: '/artwork/travel.jpg',
     text: 'Africa was only a few hours from Tarifa; one had only to cross the narrow straits by boat. Arabs often appeared in the city, shopping and chanting their strange prayers several times a day.',
     quote:
       'When each day is the same as the next, it’s because people fail to recognize the good things that happen in their lives every day that the sun rises.',
   },
   {
     name: 'The Egyptian Teahouse',
-    image: '/artwork/teahouse.png',
+    image: '/artwork/teahouse.jpg',
     text: 'The owner of the bar approached him, and the boy pointed to a drink that had been served at the next table. It turned out to be a bitter tea. The boy preferred wine.',
     quote:
       'The secret of happiness is to see all the marvels of the world, and never to forget the drops of oil on the spoon.',
   },
   {
     name: 'The Egyptian Marketplace',
-    image: '/artwork/egyptian-marketplace.png',
+    image: '/artwork/egyptian-marketplace.jpg',
     text: 'They walked together through the narrow streets of Tangier. Everywhere there were stalls with items for sale. They reached the center of a large plaza where the market was held. There were thousands of people there, arguing, selling, and buying; vegetables for sale amongst daggers, and carpets displayed alongside tobacco.',
     quote:
       'It was the language of enthusiasm, of things accomplished with love and purpose, and as part of a search for something believed in and desired.',
   },
   {
     name: "The Crystal Merchant's Store",
-    image: '/artwork/crystal-merchant.png',
+    image: '/artwork/crystal-merchant.jpg',
     text: 'The men climbed the hill, and they were tired when they reached the top. But there they saw a crystal shop that offered refreshing mint tea. They went in to drink the tea, which was served in beautiful crystal glasses.',
     quote:
       'When someone makes a decision, he is really diving into a strong current that will carry him to places he had never dreamed of when he first made the decision.',
   },
   {
     name: 'The Caravan',
-    image: '/artwork/caravan.png',
+    image: '/artwork/caravan.jpg',
     text: '“I’m the leader of the caravan,” said a dark-eyed, bearded man. “I hold the power of life and death for every person I take with me. The desert is a capricious lady, and sometimes she drives men crazy.”',
     quote:
       'People need not fear the unknown if they are capable of achieving what they need and want.',
   },
   {
     name: 'The Oasis',
-    image: '/artwork/oasis.png',
+    image: '/artwork/oasis.jpg',
     text: 'The boy awoke as the sun rose. There, in front of him, where the small stars had been the night before, was an endless row of date palms, stretching across the entire desert.',
   },
   {
     name: 'Fatima',
-    image: '/artwork/fatima.png',
+    image: '/artwork/fatima.jpg',
     text: 'At that moment, it seemed to him that time stood still, and the Soul of the World surged within him. When he looked into her dark eyes, and saw that her lips were poised between a laugh and silence, he learned the most important part of the language that all the world spoke—the language that everyone on earth was capable of understanding in their heart. It was love.',
     quote:
       'It is the hand that evokes love, and creates a twin soul for every person in the world. Without such love, one’s dreams would have no meaning.',
   },
   {
     name: 'The Pyramids',
-    image: '/artwork/pyramids.png',
+    image: '/artwork/pyramids.jpg',
     text: '',
   },
   {
     name: 'The Alchemist',
-    image: '/artwork/alchemist.png',
+    image: '/artwork/alchemist.jpg',
     text: '',
   },
 ];
